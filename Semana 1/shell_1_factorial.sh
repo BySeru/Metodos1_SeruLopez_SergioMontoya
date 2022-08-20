f953b7f2059d00018baf65402cb9d103ccbceced
sd.lopezb1 <dv.sergiobc@gmail.com>
2022-08-19 23:34:18 -0500

    ni idea, si esta vaina funcione, pero yo creo que yes

---
+++ Semana 1/shell_1_factorial.sh
@@ -0,0 +1,12 @@
+echo -n "Ingrese el número del cual desea conocer el factorial: "
+read n
+function factorial(){
+	local cont=1
+	for i in $(seq 1 $n)
+	do	
+		cont=$(($cont*$i))
+	done
+	let n_fact=$cont
+}
+factorial
+echo "El factorial de $n es: $n_fact"
