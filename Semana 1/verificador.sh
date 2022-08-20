2d57f6b95db41420b8ed085a6c3cd8ae448b0f38
sd.lopezb1 <dv.sergiobc@gmail.com>
2022-08-20 00:23:44 -0500

    Joder, esto es éstupidamente lento, pero también bonito, me alegra haberlo descubierto

--- Semana 1/verificador.sh
+++ Semana 1/verificador.sh
@@ -7,4 +7,17 @@ if ! [ $# -eq 3 ]; then
 	echo "son tres"
 	help
 	exit 1
+else
+	echo -ne 'corriendo programa                    (00%)\r'
+	sleep 1
+	echo -ne 'corriendo programa. .                 (20%)\r'
+	sleep 1
+	echo -ne 'corriendo programa. . . .             (40%)\r'
+	sleep 1
+	echo -ne 'corriendo programa. . . . . .         (60%)\r'
+	sleep 1
+	echo -ne 'corriendo programa. . . . . . . .     (80%)\r'
+	sleep 1
+	echo -ne 'corriendo programa. . . . . . . . . . (100%)\r'
+	echo -ne '\n'
 fi
